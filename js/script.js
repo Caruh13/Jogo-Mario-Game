d3abc2dd5b00dbfeda9dd25a5a221f1d4a00f6e9
Carol Messias <carol.messias2020@gmail.com>
2022-05-31 11:45:31 -0300

    MarioGame

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const mario = document.querySelector('.mario')
+const pipe = document.querySelector('.pipe')
+const ojogo = document.querySelector('ojogo')
+
+const jump = () => {
+  mario.classList.add('jump')
+
+  setTimeout(() => {
+    mario.classList.remove('jump')
+  }, 500)
+}
+
+const loop = setInterval(() => {
+  const pipePosition = pipe.offsetLeft
+  const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '')
+
+  if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+    pipe.style.animation = 'none'
+    pipe.style.left = `${pipePosition}px`
+
+    mario.style.animation = 'none'
+    mario.style.bottom = `${marioPosition}px`
+
+    mario.src = './images/ojogo.png'
+
+    mario.style.width = '500px'
+    mario.style.marginLeft = '300px'
+
+    clearInterval(loop)
+  }
+}, 10)
+
+document.addEventListener('keydown', jump)
